5ae1d76e1abe991de112f936f8837965c8855c82
zhanminezhan <zhanminezhan@gmail.com>
2025-03-24 14:13:29 +0500

    preparing for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/test-landing-sit/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
